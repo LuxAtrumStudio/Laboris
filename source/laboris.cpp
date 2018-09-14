e7ff82d4df0ce4cad79a9b9990917494d80e0988
Arden Rasmussen <ardenisthebest@gmail.com>
2018-09-13 21:44:00 -0700

    Began major local rewrite

--- source/laboris.cpp
+++
@@ -1,9 +0,0 @@
-#include "laboris.hpp"
-
-#include <vector>
-
-#include "task.hpp"
-
-namespace laboris {
-  std::vector<laboris::Task> global_tasks_, completed_tasks_, tmp_task_list_;
-}  // namespace laboris
